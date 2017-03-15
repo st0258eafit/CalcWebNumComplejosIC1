58fbbd79d5d6cc22135b3ae7203c2df3c9da0c0c
edwinmontoya <edwin.montoya@gmail.com>
2017-03-15 11:52:39 -0500

    1st commit

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author AULA MOVIL
+ */
+public class Calculator {
+        public int sumar(int a, int b) {
+        return a+b;
+    }
+
+    public int restar(int a, int b) {
+        return a-b;
+    }
+    
+    public int multiplicar(int a, int b) {
+        return a*b;
+    }
+
+    public int dividir(int a, int b) {
+        return a/b;
+    }
+}
